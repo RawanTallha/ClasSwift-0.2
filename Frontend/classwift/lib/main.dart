0e95436e6837dd98810922d752bcd28dfdeadd0f
Hams Assiri <hams.a.b.assiri@hotmail.com>
2024-11-18 11:47:10 +0300

    Maintenance View ( Mock data & API )

--- Frontend/classwift/lib/main.dart
+++ Frontend/classwift/lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:classwift/pages/Availability_Page.dart';
+import 'package:classwift/pages/MaintenanceMock.';
+import 'package:classwift/pages/MaintenanceView';
 import 'package:classwift/pages/NavigationBarScreen.dart';
 import 'package:classwift/pages/Users_page.dart';
+import 'package:classwift/pages/contact_page.dart';
+import 'package:classwift/pages/home_page.dart';
 import 'package:classwift/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 
@@ -22,7 +27,10 @@ class MainApp extends StatelessWidget {
       //home: DemoPage(),
       //home: ProfilePage()
       //home: ReportPage()
-      home:WelcomePage()
+      //home:WelcomePage()
+      //home:ContactUsPage()
+      //home:MaintenanceView()
+      home:MaintenanceMock()
     );
   }
 }
